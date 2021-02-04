f01d19f9b85e842f7dc5500f5ab1c57888e95db1
anthonycervi <anthonycervi97gmail.com>
2021-02-03 20:48:02 -0800

    Created Template for BusyBee, added comps and page

---
+++ src/pages/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Template from '../../comps/Template';
+
+const Home = () =>{
+    return  <div className="main">
+                <Template />
+            </div>
+}
+
+export default Home;
