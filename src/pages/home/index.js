6db5721e9b4ba83935b55ea7c4078920dba5df3b
Josh Glacier <josh_mountain@live.com>
2021-02-09 17:06:47 -0800

    CSS Configs

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -21,66 +21,98 @@ const fakedb = [
         TitleText:"Take Susie to School",
         DriverText:"Sarah",
         VehicleText:"Honda Civic",
-        TimeText:"9:45AM"
+        TimeText:"9:45AM",
+        namecolor:"#0000FF",
+        vehiclecolour:"tomato"
     },
     {
         TitleText:"Take Tim to School",
         DriverText:"Dad",
-        VehicleText:"Tesla"
+        VehicleText:"Tesla",
+          namecolor:"#0000FF",
+        vehiclecolour:"tomato"
     },
     {
         TitleText:"Take Jimmy himmy to School",
         DriverText:"Henry",
         VehicleText:"Lambo"
+        ,
+          namecolor:"#0000FF",
+        vehiclecolour:"tomato"
     },
     {
         TitleText:"Take Tom to School",
         DriverText:"Dave",
         VehicleText:"Buggy"
+        ,
+          namecolor:"#0000FF",
+        vehiclecolour:"tomato"
     },
     {
         TitleText:"Take Barry B Benson to the Hive",
         DriverText:"Dad",
         VehicleText:"Plane"
+        ,
+          namecolor:"#0000FF",
+        vehiclecolour:"tomato"
     },
     {
         TitleText:"Take Bon. Eless Pizza to School",
         DriverText:"Dave",
         VehicleText:"Honda Civic"
+        ,
+          namecolor:"#0000FF",
+        vehiclecolour:"tomato"
     },
     {
         TitleText:"Pick up Rob",
         DriverText:"Sally",
         VehicleText:"Ferrari"
+        ,
+          namecolor:"#0000FF",
+        vehiclecolour:"tomato"
     },
     {
         TitleText:"Take Tom to Practice",
         DriverText:"Dave",
         VehicleText:"Porsche"
+        ,
+          namecolor:"magenta",
+        vehiclecolour:"green"
     }
 ]    
 //Preview Components on Page; Title, Time, Driver Name, Vehicle Name, Hour,Minute,Meridian: [WORKING]
     const [TitleText,setTitle] = useState([]);
-    const [TimeText,setTime] = useState([]);
+    const [HourTime,setHourTime] = useState([]);
+    const [MinuteTime,setMinuteTime] = useState([]);
+    const [MeridianTime,setMeridianTime] = useState([]);
     const [DriverText,setDriver] = useState([]);
     const [VehicleText,setVehicle] = useState([]);
     const [DateText,setDateText] = useState([]);
     const [alldb, setAllDb] = useState([]);
+    const [namecolor, setnamecolor] = useState([]);
+    const [vehiclecolour, setvehiclecolour] = useState([]);
+   
+    var ShowTask = false;
   
 //Async get DB [WILL BE REPLACED WITH REAL DATABASE LINK]
     const GetDB = async() => {
-        var arr = fakedb.slice(0,4);
+        var arr = fakedb.slice(0,8);
         setAllDb(arr)
     }
 
     //Handles information from the form to update preview. [ADD TO DB FROM SAM]
-    const HandleFormComplete = async ({TitleText,TimeText,DriverText,VehicleText,compDate}) => {
+    const HandleFormComplete = async ({TitleText, HourTime, MinuteTime, MeridianTime, DriverText,VehicleText,compDate, ColourValue, VehicleNameColour}) => {
     setTitle(TitleText);
-    setTime(TimeText);
+    setHourTime(HourTime);
+    setMinuteTime(MinuteTime);
+    setMeridianTime(MeridianTime)
     setDriver(DriverText);
     setVehicle(VehicleText);
     setDateText(compDate);
-    console.log(compDate)
+    console.log(ColourValue)
+    setnamecolor(ColourValue)
+    setvehiclecolour(VehicleNameColour)
     }
 
     //Filter 1 Vehicle, needs to be integrated into real database.
@@ -95,7 +127,7 @@ const fakedb = [
 
     //Filter 1 Name, needs to be integrated into real database.
     const OnFilterName = (text) => {
-        setAllDb(alldb.filter((o)=>{
+        setAllDb(fakedb.filter((o)=>{
           return o.DriverText.includes(text);
         })
         )
@@ -113,20 +145,23 @@ const fakedb = [
     <div className="main">
                 <Header></Header>
                
-                <FilterBy onFilterVehicle={OnFilterVehicle}></FilterBy>
-             
+                <FilterBy onFilterVehicle={OnFilterVehicle} onFilterName={OnFilterName}></FilterBy>
+                
               <div className="ScrollDiv">
-                    <TaskCard DateProps={DateText}> </TaskCard>
+               
                   {/* Maps out the fake database and assigns those values directly to the comps */}
 
                 {alldb.map(o=>{
-                    return <TaskCard TitleText={o.TitleText} DriverText={o.DriverText} VehicleText={o.VehicleText}>
-                        {o.TitleText} - {o.VehicleText} - {o.DriverText}
+                    return <TaskCard namecolor={o.namecolor} vehiclecolor={o.vehiclecolour} DateText={o.DateText}HourText={o.HourTime} MinuteText={o.MinuteTime} MeridianText={o.MeridianTime} TitleText={o.TitleText} DriverText={o.DriverText} VehicleText={o.VehicleText}>
+                        {o.TitleText} - {o.VehicleText} - {o.DriverText} - {o.HourText} - {o.MinuteText} - {o.MeridianText} - {o.DateText}
                     </TaskCard>
                 })}
                 </div > 
                 <div className="Button1">
-                    <Button2 ></Button2>   
+                    <Button2  onClick={()=>{
+                    ShowTask(true)
+                }}></Button2>
+           
                 </div>           
                 </div>
 
@@ -134,8 +169,9 @@ const fakedb = [
 
             <FadeIn delay={150} transitionDuration={1600}>
             <div className="card" style={{padding, left, top,position:'relative'}}>
-            <AddTaskCard onPreview={HandleFormComplete} onPress={console.log(TitleText)}></AddTaskCard>
+       
             </div>
+            <AddTaskCard onPreview={HandleFormComplete} onPress={console.log(TitleText)}></AddTaskCard>
             </FadeIn>
             </div>
             </FadeIn>
