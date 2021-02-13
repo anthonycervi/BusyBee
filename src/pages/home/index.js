8beb9cc7e04a59e2a398a604e5c22ab9964b14d7
Josh Glacier <josh_mountain@live.com>
2021-02-12 17:56:26 -0800

    Final Project Push Josh Anthony Veryna

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -11,78 +11,45 @@ import FilterBy from '../../comps/FilterBy'
 import FadeIn from 'react-fade-in';
 import AddTaskCard from '../../comps/AddTaskCard'
 import EditTaskCard from '../../comps/EditTaskCard'
+import BeeImage from '../../images/bee.png';
+import styled from 'styled-components';
+
+const BeeImage2 = styled.img`
+width:70px;
+height:70px;
+`;
 
 //This page emulates a mobile interface
 
 const Home = () =>{
-// Preview / Updates Practices
 //Fake Database with Information on the Title, Driver, Vehicle, etc.
-const fakedb = [
-    {
-        TitleText:"Take Susie to School",
-        DriverText:"Sarah",
-        VehicleText:"Honda Civic",
-        TimeText:"9:45AM",
-        namecolor:"#0000FF",
-        vehiclecolour:"tomato"
-    },
-    {
-        TitleText:"Take Tim to School",
-        DriverText:"Dad",
-        VehicleText:"Tesla",
-          namecolor:"#0000FF",
-        vehiclecolour:"tomato"
-    },
-    {
-        TitleText:"Take Jimmy himmy to School",
-        DriverText:"Henry",
-        VehicleText:"Lambo"
-        ,
-          namecolor:"#0000FF",
-        vehiclecolour:"tomato"
-    },
-    {
-        TitleText:"Take Tom to School",
-        DriverText:"Dave",
-        VehicleText:"Buggy"
-        ,
-          namecolor:"#0000FF",
-        vehiclecolour:"tomato"
-    },
-    {
-        TitleText:"Take Barry B Benson to the Hive",
-        DriverText:"Dad",
-        VehicleText:"Plane"
-        ,
-          namecolor:"#0000FF",
-        vehiclecolour:"tomato"
-    },
-    {
-        TitleText:"Take Bon. Eless Pizza to School",
-        DriverText:"Dave",
-        VehicleText:"Honda Civic"
-        ,
-          namecolor:"#0000FF",
-        vehiclecolour:"tomato"
-    },
-    {
-        TitleText:"Pick up Rob",
-        DriverText:"Sally",
-        VehicleText:"Ferrari"
-        ,
-          namecolor:"#0000FF",
-        vehiclecolour:"tomato"
-    },
-    {
-        TitleText:"Take Tom to Practice",
-        DriverText:"Dave",
-        VehicleText:"Porsche"
-        ,
-          namecolor:"magenta",
-        vehiclecolour:"green"
-    }
-]    
-//Preview Components on Page; Title, Time, Driver Name, Vehicle Name, Hour,Minute,Meridian: [WORKING]
+// const fakedb = [
+//     {
+//         TitleText:"Take Susie to School",
+//         DriverText:"Sarah",
+//         VehicleText:"Honda Civic",
+//         TimeText:"9:45AM",
+//         namecolor:"#0000FF",
+//         vehiclecolour:"tomato"
+//     },
+//     {
+//         TitleText:"Take Tim to School",
+//         DriverText:"Dad",
+//         VehicleText:"Tesla",
+//           namecolor:"#0000FF",
+//         vehiclecolour:"tomato"
+//     },
+//     {
+//         TitleText:"Take Jimmy himmy to School",
+//         DriverText:"Henry",
+//         VehicleText:"Lambo"
+//         ,
+//           namecolor:"#0000FF",
+//         vehiclecolour:"tomato"
+//     }
+// ]    
+
+//Preview Components on Page; Title, Time, Driver Name, Vehicle Name, Hour,Minute,Meridian
     const [TitleText,setTitle] = useState([]);
     const [HourTime,setHourTime] = useState([]);
     const [MinuteTime,setMinuteTime] = useState([]);
@@ -97,12 +64,7 @@ const fakedb = [
    
     var ShowTask = false;
   
-//Async get DB [WILL BE REPLACED WITH REAL DATABASE LINK]
-    // const GetDB = async() => {
-    //     var arr = fakedb.slice(0,8);
-    //     setAllDb(arr)
-    // }
-    //Handles information from the form to update preview. [ADD TO DB FROM SAM]
+//Handle Form Complete from Page Components.
     const HandleFormComplete = async ({TitleText, HourTime, MinuteTime, MeridianTime, DriverText,VehicleText,compDate, ColourValue, VehicleNameColour}) => {
     setTitle(TitleText);
     setHourTime(HourTime);
@@ -116,6 +78,7 @@ const fakedb = [
     setvehiclecolour(VehicleNameColour)
     }
 
+    //Async call to database and stores as const usestates
     const GetDB = async() => {
       var resp = await axios.get("https://comp4130busybee.herokuapp.com/api/tasks")
       setRespDB(resp.data);
@@ -124,19 +87,27 @@ const fakedb = [
       console.log(resp.data.tasks)
       }
 
+      //Async call to post a new task to the database
       const HandlePost = async (TitleText, DateText, DriverText, VehicleText, HourText, MinuteText, MeridianText, namecolor, vehiclecolour) => {
         var resp = await axios.post("https://comp4130busybee.herokuapp.com/api/tasks", {TitleText:TitleText, DateText:DateText, DriverText:DriverText, VehicleText:VehicleText, HourText:HourText, MinuteText:MinuteText, MeridianText:MeridianText, namecolor:namecolor, vehiclecolour:vehiclecolour});
         console.log("created", resp.data)
         GetDB();
         }
 
+        //Async call to patch/edit Task #1 / ID 171 in the database
         const EditPost = async (TitleText, DateText, DriverText, VehicleText, HourText, MinuteText, MeridianText, namecolor, vehiclecolour) => {
-          var resp = await axios.patch("https://comp4130busybee.herokuapp.com/api/tasks/1", {TitleText:TitleText, DateText:DateText, DriverText:DriverText, VehicleText:VehicleText, HourText:HourText, MinuteText:MinuteText, MeridianText:MeridianText, namecolor:namecolor, vehiclecolour:vehiclecolour});
+          var resp = await axios.patch("https://comp4130busybee.herokuapp.com/api/tasks/171", {TitleText:TitleText, DateText:DateText, DriverText:DriverText, VehicleText:VehicleText, HourText:HourText, MinuteText:MinuteText, MeridianText:MeridianText, namecolor:namecolor, vehiclecolour:vehiclecolour});
           console.log("created", resp.data)
           GetDB();
           }
+          //Async call to delete Task #2 / ID 181 from database
+          const DeletePost = async (TitleText, DateText, DriverText, VehicleText, HourText, MinuteText, MeridianText, namecolor, vehiclecolour) => {
+            var resp = await axios.delete("https://comp4130busybee.herokuapp.com/api/tasks/181", {TitleText:TitleText, DateText:DateText, DriverText:DriverText, VehicleText:VehicleText, HourText:HourText, MinuteText:MinuteText, MeridianText:MeridianText, namecolor:namecolor, vehiclecolour:vehiclecolour});
+            console.log("deleted", resp.data)
+            GetDB();
+            }
 
-    //Filter 1 Vehicle, needs to be integrated into real database.
+    //Filter 1 Vehicle, 
     const OnFilterVehicle = (text) => {
         //console.log(text, alldb);
         setAllDb(alldb.filter((o)=>{
@@ -146,7 +117,7 @@ const fakedb = [
         )
       }
 
-    //Filter 1 Name, needs to be integrated into real database.
+    //Filter 1 Name
     const OnFilterName = (text) => {
         setAllDb(alldb.filter((o)=>{
           return o.DriverText.includes(text);
@@ -156,7 +127,7 @@ const fakedb = [
 
 
      
-    //Will be replaced with real DB async
+    //UseEffect to run the GetDB function
       useEffect(()=>{
         GetDB()
       },[]);
@@ -169,15 +140,19 @@ const fakedb = [
                 <AddTaskCard onFormComplete={HandlePost} onPreview={HandleFormComplete} onPress={console.log(TitleText)} ></AddTaskCard>
                 </div>
                 <div className="div2">
+                  <div className="LogoTitleDiv">
+                    <div className="TitleDiv">Busy</div>
+                    <div className="LogoDiv"> <BeeImage2 src={BeeImage}/> </div>
+                  </div>
                 <div className="main">
                       <Header></Header>
                       <FilterBy onFilterVehicle={OnFilterVehicle} onFilterName={OnFilterName}></FilterBy>
                 
                       <div className="ScrollDiv">             
-                        {/* Maps out the fake database and assigns those values directly to the comps */}
+                        {/* Maps out the database and assigns those values directly to the comps */}
 
                         {alldb.map(o=>{
-                            return <TaskCard namecolor={o.namecolor} vehiclecolor={o.vehiclecolour} DateText={o.DateText} HourText={o.HourText} MinuteText={o.MinuteText} MeridianText={o.MeridianText} TitleText={o.TitleText} DriverText={o.DriverText} VehicleText={o.VehicleText}>
+                            return <TaskCard onDelete={DeletePost} namecolor={o.namecolor} vehiclecolor={o.vehiclecolour} DateText={o.DateText} HourText={o.HourText} MinuteText={o.MinuteText} MeridianText={o.MeridianText} TitleText={o.TitleText} DriverText={o.DriverText} VehicleText={o.VehicleText}>
                                 {o.TitleText} - {o.VehicleText} - {o.DriverText} - {o.DateText} - {o.HourText} - {o.MinuteText} - {o.MeridianText} 
                             </TaskCard>
                         })}
@@ -194,34 +169,6 @@ const fakedb = [
                 </div>
               </div>
     
-    // <div className="EntirePage"> 
-    //                 <div className="main">
-    //                   <Header></Header>
-    //                   <FilterBy onFilterVehicle={OnFilterVehicle} onFilterName={OnFilterName}></FilterBy>
-                
-    //                   <div className="ScrollDiv">             
-    //                     {/* Maps out the fake database and assigns those values directly to the comps */}
-
-    //                     {alldb.map(o=>{
-    //                         return <TaskCard namecolor={o.namecolor} vehiclecolor={o.vehiclecolour} DateText={o.DateText} HourText={o.HourText} MinuteText={o.MinuteText} MeridianText={o.MeridianText} TitleText={o.TitleText} DriverText={o.DriverText} VehicleText={o.VehicleText}>
-    //                             {o.TitleText} - {o.VehicleText} - {o.DriverText} - {o.DateText} - {o.HourText} - {o.MinuteText} - {o.MeridianText} 
-    //                         </TaskCard>
-    //                     })}
-    //                   </div >
-    //                   <div className="Button1">
-    //                       <Button2  onClick={()=>{
-    //                       ShowTask(true)
-    //                   }}></Button2>
-    //                   </div>           
-    //                 </div>
-                
-    //             <div className="AddTaskDiv">
-    //             <AddTaskCard onFormComplete={HandlePost} onPreview={HandleFormComplete} onPress={console.log(TitleText)} ></AddTaskCard>
-    //             </div>
-    //             <div className="EditTaskDiv">
-    //             <EditTaskCard onFormComplete={EditPost} /> 
-    //             </div>
-    //           </div>  
               }
 
 export default Home;
